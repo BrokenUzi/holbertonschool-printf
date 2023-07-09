8a66dd25e75a1d6232ac67c2cc45c6303ed5397f
root <jcup143@gmail.com>
2023-07-08 17:51:13 -0700

    Added strlen.c file, updated main.h (not complete)

--- main.h
+++ main.h
@@ -1,7 +1,17 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+/**
+* 
+*
+*
+*/
+
 int _printf(const char *format, ...);
 int _putchar(char);
 int _strlen(char *s);
+int c_print(va_list);
+int s_print(va_list);
+int d_print(va_list);
+
 #endif
