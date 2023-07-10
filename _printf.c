e0fe96bf35144ebc60ab0bb02b785e0e21bc0602
abrielleperry <abrielleperry22@icloud.com>
2023-07-10 12:17:11 -0700

    chmod _printf.c

--- _printf.c
+++ _printf.c
@@ -10,11 +10,16 @@ int _printf(const char *format, ...);
 	va_list, varg
 	va_start (varg format);
 	int fmtType[] = {
-	{%d, d_printf},
-	{%i, i_printf},
-	{%c, c_printf},
-	{%s, s_printf},
+	{'d', i_printf},
+	{'i', i_printf},
+	{'c', c_printf},
+	{'s', s_printf},
 	{'/0', Null}
 };
 int len = _strlen
-for (int i = 0; i < strlen(format); i++);
+for (int i = 0; i < _strlen(format); i++)
+{
+if (format[i] == 'd')
+}	int val = va_arg(varg int);
+	d_printf(val);
+
