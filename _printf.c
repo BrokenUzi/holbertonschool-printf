9e584b9db21efdec86e559159dd8bf58604e174c
abrielleperry <abrielleperry22@icloud.com>
2023-07-10 11:49:28 -0700

    creating _printf.c file

---
+++ _printf.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _printf - 
+ * varg - 
+ * Return
+ */
+int _printf(const char *format, ...);
+{
+	va_list, varg
+	va_start (varg format);
+	int fmtType[] = {
+	{%d, d_printf},
+	{%i, i_printf},
+	{%c, c_printf},
+	{%s, s_printf},
+	{'/0', Null}
+};
+int len = _strlen
+for (int i = 0; i < strlen(format); i++);
