92a31715a5c0a1e3558751daae604ab411491869
abrielleperry <abrielleperry22@icloud.com>
2023-07-10 13:57:44 -0700

    updating printf

--- _printf.c
+++ _printf.c
@@ -7,19 +7,25 @@
  */
 int _printf(const char *format, ...);
 {
+	
 	va_list, varg
-	va_start (varg format);
-	int fmtType[] = {
-	{'d', i_printf},
-	{'i', i_printf},
-	{'c', c_printf},
-	{'s', s_printf},
-	{'/0', Null}
+	fmtType array[] = {
+		{'d', i_printf},
+		{'i', i_printf},
+		{'c', c_printf},
+		{'s', s_printf},
+		{'\0', NULL}
 };
+
+
 int len = _strlen
+
+	va_start(varg, format);
+	if (format == NULL)
+		return (-1);
 for (int i = 0; i < _strlen(format); i++)
 {
-if (format[i] == 'd')
+	if (format[i] == 'd')
 }	int val = va_arg(varg int);
 	d_printf(val);
 
