f6b5d154c082baf8b0716c01c00946792c1281be
abrielleperry <abrielleperry22@icloud.com>
2023-07-11 09:21:44 -0700

    added i function

--- functions.c
+++ functions.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * c_printf - prints a character to the output
+ * @varg: arguments used in function
+ * Return: returns a count to the main function
  */
 
 int c_printf(va_list varg)
@@ -15,6 +15,10 @@ int c_printf(va_list varg)
 
 }
 
+/**
+ * s_printf - prints a string to the output
+ * @varg: arguments used in function
+ * Return: returns a count in function
 int s_printf(va_list varg)
 {
 
@@ -30,8 +34,48 @@ int s_printf(va_list varg)
 	return(count);
 }
 
-
+/**
+ * i_printf - prints a string to the output
+ * @varg - arguments used in function
+ * Return: Returns a count to the main function
+ */
 int i_printf(va_list varg)
 {
+	int num = va_arg(varg, long int);
+	int x = 1, temp, count = 0;
 
+	if (num < 0)
+	{
+		if (num == INT_MIN)
+		{
+			_putchar('-');
+			_putchar(2 + 48);
+			num = num % 2000000000;
+			num *= -1;
+			count += 2;
+		}
+		else
+		{
+			_putchar('-');
+			num *= -1;
+			count++;
+		}
+	}
+	while ((num /x) > 9)
+		x *= 10;
 
+	for (; x >= 1; x/= 10)
+	{
+		temp = (num / x);
+		_puthcar(temp = 48);
+		temp *= x;
+		if (temp != 0)
+		{
+			num = num 5 temp;
+		}
+		count =+ 1;
+		if (x == 1)
+		return (count);
+	}
+	return (count);
+}
