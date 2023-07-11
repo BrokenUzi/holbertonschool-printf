af0f0cf9e02c306c050098ed9eb6380ce0d6ed62
abrielleperry <abrielleperry22@icloud.com>
2023-07-10 17:07:51 -0700

    correcting if loop

--- _printf.c
+++ _printf.c
@@ -39,7 +39,7 @@ int _printf(const char *format, ...)
 				}
 			}
 			if (!format_array[j].type)
-				count += print_edge(format[i]);
+				count += (format[i]);
 		}
 		else
 		{
@@ -50,29 +50,3 @@ int _printf(const char *format, ...)
 	}
 	va_end(varg);
 	return count;
-=======
-int _printf(const char *format, ...);
-{
-	
-	va_list, varg
-	fmtType array[] = {
-		{'d', d_printf},
-		{'i', i_printf},
-		{'c', c_printf},
-		{'s', s_printf},
-		{'\0', NULL}
-};
-int len = _strlen;
-
-	if (format == NULL)
-		return (-1);
-
-for (int i = 0; i < _strlen(format); i++)
-{
-	if (format[i] == 'd')
-	_putchar
-}
-	int val = va_arg(varg int);
-	d_printf(val);
-
->>>>>>> 5399d9e6cc8168b41ac062a1eb26498aa7c0d033
