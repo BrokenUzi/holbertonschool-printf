c5afc7991f95254146e08a28a8d72376a8221459
abrielleperry <abrielleperry22@icloud.com>
2023-07-11 10:14:01 -0700

    adding includes to main

--- main.h
+++ main.h
@@ -1,11 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-/**
-* struct test - structure called test bc its a test
-* 
-* 
-*/
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
 
 typedef stuct arg
 {
@@ -16,7 +15,6 @@ typedef stuct arg
 int _printf(const char *format, ...);
 int _putchar(char);
 int _strlen(char *s);
-int d_print(va_list);
 int i_print(va_list);
 int c_print(va_list);
 int s_print(va_list);
