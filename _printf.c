0663b284788ed801c8796fc182228e5f93f5c065
abrielleperry <abrielleperry22@icloud.com>
2023-07-11 16:26:32 -0700

    more betty fixes

--- _printf.c
+++ _printf.c
@@ -11,7 +11,7 @@ int _printf(const char *format, ...)
 {
 	int i = 0, count = 0, j; /* Initialize counters and temporary variable j */
 	va_list varg;
-	fmtType format_array[] = { /* Define the structure array for handling different format specifiers */
+	fmtType format_array[] = { /*Define structarray handling diff formatspecifs*/
 			{"d", i_printf},
 			{"i", i_printf},
 			{"c", c_printf},
@@ -19,25 +19,25 @@ int _printf(const char *format, ...)
 			{"\0", NULL} /* NULL terminator for the array */
 	};
 
-	va_start(varg, format); /* Initialize the variable argument list with format as last named arg */
+	va_start(varg, format); /*Initialize the varglist w format as last named arg*/
 	if (!format) /* Check if format is NULL, if so return -1 as error code */
 		return (-1);
-	while (format && format[i]) /* Iterate through the format string until NULL character is encountered */
+	while (format && format[i]) /*Iterate thru format str til NULL char*/
 	{
-		if (format[i] == '%') /* If a % is encountered a format specifier is expected */
+		if (format[i] == '%') /*If % is encountered format specif is expected*/
 		{
-			if (!format[i + 1]) /* If the format string ends after a % return -1 as error */
+			if (!format[i + 1]) /*If format str ends after % return -1 as error*/
 				return (-1);
 			++i; /* Move to the next character which should be the format specifier */
-			for (j = 0; format_array[j].type; ++j) /* Iterate through the array of format specifier handlers */
+			for (j = 0; format_array[j].type; ++j) /*Iteratethruarrayfrmtspecif hndlrs*/
 			{
-				if (format[i] == *(format_array[j].type)) /* If the current character matches a type in the array */
+				if (format[i] == *(format_array[j].type)) /*If currnt char = type n array*/
 				{
-					count += format_array[j].f(varg); /* Call the corresponding handler function and add its return value to the count */
+					count += format_array[j].f(varg); /*Call hndlr func+return val to count*/
 					break; /* Break the loop since we found a match */
 				}
 			}
-			if (!format_array[j].type) /* If no match was found in the array its an unsupported format specifier */
+			if (!format_array[j].type) /*If no match found in array not format specif*/
 				count += e_printf(format[i]);
 		}
 		else
